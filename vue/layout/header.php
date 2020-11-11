8109f3971027c7e437e28939697249e6200f35d8
Nahmani <benjamin.nahmani770@gmail.com>
2020-11-12 00:41:31 +0100

    Début du mobile first
    Refonte du CSS en mobile first (début)

--- vue/layout/header.php
+++ vue/layout/header.php
@@ -2,6 +2,8 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
@@ -12,19 +14,41 @@
     <link rel="stylesheet" href="/style.css">
 </head>
 <body>
+
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+      <a class="navbar-brand" href="/home"><img src="image/icon.png" alt="Logo du site" id="main_logo"></a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+
+        <span class="navbar-toggler-icon"></span>
+
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+
+        <div class="navbar-nav">
+
+          <a class="nav-item nav-link active" href="/home" class="button">Episodes</a>
+
+          <a class="nav-item nav-link" href="/admin/billet/liste" class="button">Administration</a>
+        </div>
+      </div>
+    </nav>
+  </header> 
     
-    <nav class="navbar">
+ <!--    <nav class="navbar">
                 <div id="logo">
                   <a href="/home"><img src="/image/icon.png" alt="Le Logo du site" id="img_logo"></a>
                 </div>
 
-
                 <ul id="list_nav">
+
 
                     <li><a href="/home" class="button">Acceuil</a></li>
                     <li><a href="#" class="button">Episodes</a></li>
                     <li><a href="/admin/billet/liste" class="button">Espace admin</a> </li>
 
                 </ul>
-
-    </nav>
+    </nav> -->
